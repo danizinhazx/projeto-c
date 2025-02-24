1a8fa389da1866f3ee17994d51c7c00d708061b2
danizinhazx <danisouza.2717@gmail.com>
2025-02-23 23:28:00 -0300

    Adicionando estrutura inicial do sistema

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+void exibirMenu() {
+    printf("====================================\n");
+    printf("      Sistema de Cadastro em C      \n");
+    printf("====================================\n");
+    printf("1 - Cadastrar usuário\n");
+    printf("2 - Listar usuários\n");
+    printf("3 - Sair\n");
+    printf("Escolha uma opção: ");
+}
+
 int main() {
-    printf("Olá, mundo! Este é o início do projeto em C.\n");
+    int opcao;
+    
+    do {
+        exibirMenu();
+        scanf("%d", &opcao);
+        
+        switch (opcao) {
+            case 1:
+                printf("Função de cadastro ainda será implementada.\n");
+                break;
+            case 2:
+                printf("Função de listagem ainda será implementada.\n");
+                break;
+            case 3:
+                printf("Saindo do sistema...\n");
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+    } while (opcao != 3);
+
     return 0;
 }
